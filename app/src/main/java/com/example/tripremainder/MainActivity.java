335ac5d95550f68f2578f331f689547ea03910ad
hagersamir123 <hagersamir477@gmail.com>
2021-03-18 19:21:49 -0700

    frist commit

--- app/src/main/java/com/example/tripremainder/MainActivity.java
+++ app/src/main/java/com/example/tripremainder/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.tripremainder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
