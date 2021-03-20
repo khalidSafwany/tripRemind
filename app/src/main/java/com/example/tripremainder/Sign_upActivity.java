b0418f4eb66ff7c852c583f9c911836376f71676
hagersamir123 <hagersamir477@gmail.com>
2021-03-20 12:09:16 -0700

    signIn and SigUp screans

---
+++ app/src/main/java/com/example/tripremainder/Sign_upActivity.java
@@ -0,0 +1,107 @@
+package com.example.tripremainder;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Sign_upActivity extends AppCompatActivity {
+
+    private EditText name, email_id, passwordcheck;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "";
+    private ProgressBar progressBar;
+    private Button signupbtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+        TextView btnSignUp = (TextView) findViewById(R.id.login_page);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Sign_upActivity.this, Sign_inActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        email_id = (EditText) findViewById(R.id.input_email);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        passwordcheck = (EditText) findViewById(R.id.input_password);
+        signupbtn= (Button) findViewById(R.id.btn_signup);
+
+
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = email_id.getText().toString();
+                String password = passwordcheck.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter your Eamil", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Sign_upActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                progressBar.setVisibility(View.GONE);
+
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(Sign_upActivity.this, "Authentication success.",
+                                            Toast.LENGTH_SHORT).show();
+//                                    Intent intent = new Intent(Sign_upActivity.this, Home_screen.class);
+//                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(Sign_upActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+
+                            }
+
+
+                        });
+            }
+        });
+
+
+
+    }
+}
