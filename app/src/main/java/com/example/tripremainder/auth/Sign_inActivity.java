d729e7a86a5cee94cb44159963c32adbf5eaba9b
Azhar-ragab <azharragab9@gmail.com>
2021-03-21 14:53:52 +0200

    create landscape screen

--- app/src/main/java/com/example/tripremainder/auth/Sign_inActivity.java
+++ app/src/main/java/com/example/tripremainder/auth/Sign_inActivity.java
@@ -1,4 +1,4 @@
-package com.example.tripremainder;
+package com.example.tripremainder.auth;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +14,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.tripremainder.R;
 import com.example.tripremainder.home.HomeActivity;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
--- app/src/main/java/com/example/tripremainder/auth/Sign_upActivity.java
+++ app/src/main/java/com/example/tripremainder/auth/Sign_upActivity.java
@@ -1,4 +1,4 @@
-package com.example.tripremainder;
+package com.example.tripremainder.auth;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +14,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.tripremainder.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
